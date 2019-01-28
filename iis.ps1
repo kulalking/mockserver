46f98c7e1120c561d3264eff8fefa357035760cb
kulalking <sathish.kulal@gmail.com>
2019-01-27 23:59:19 -0800

    Update iis.ps1

--- iis.ps1
+++ iis.ps1
@@ -1,2 +1,2 @@
 Add-WindowsFeature Web-Server
-Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "service:1:machineName: $($env:computername) !"
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "service:1:machineName: $($env:computername) !somethingOLD"
