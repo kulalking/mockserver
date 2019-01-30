d814d054cd9cd1e9c632f2cd9bc233cb7808654e
kulalking <sathish.kulal@gmail.com>
2019-01-30 11:49:00 -0800

    Update iis.ps1

--- iis.ps1
+++ iis.ps1
@@ -1,2 +1,2 @@
 Add-WindowsFeature Web-Server
-Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "service:1:machineName: $($env:computername) version:2.1"
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "service:1:machineName: $($env:computername) version:3.1"
